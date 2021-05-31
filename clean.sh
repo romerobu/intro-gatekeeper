c83c499284e062e9e8c0a961373bd76732593949
Coral Romero Buzon <cromerob@cromerob.remote.csb>
2021-05-31 22:29:32 +0200

    Update readme

--- clean.sh
+++ clean.sh
@@ -8,4 +8,5 @@ oc delete constraintTemplate k8smaxrequests
 oc delete constraintTemplate k8srequiredlabels
 oc delete gatekeeper gatekeeper -n gatekeeper-system
 oc delete subscription gatekeeper-operator-product -n openshift-operators
+oc delete csv gatekeeper-operator-product.v0.1.2 -n openshift-operators
 oc delete project gatekeeper-project gatekeeper-project-excluded gatekeeper-system
