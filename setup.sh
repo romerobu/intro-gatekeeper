dcf1a17f420d2d8e9869d30ee07efe39174ab307
Coral Romero Buzon <cromerob@cromerob.remote.csb>
2021-06-01 00:40:58 +0200

    Update readme

--- setup.sh
+++ setup.sh
@@ -22,26 +22,34 @@ oc apply -f config/install-operator.yaml
 echo -n "Waiting for pods ready..."
 while [[ $(oc get pods  -n openshift-operators  -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
 
+sleep 5
 
 # Create gatekeeper instance
 
 echo -n "Creating gatekeeper..."
 oc apply -f config/create-gatekeeper.yaml
 
+echo -n "Waiting for pods ready..."
+while [[ $(oc get pods  -n openshift-gatekeeper-system  -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
+
+sleep 5
+
 # Deploy gatekeeper constraints and templates
 
 echo -n "Deploying constraints..."
 oc apply -f config/config.yaml
 
-oc apply -f constraints/K8sMaxPods.yaml
 oc apply -f constraintTemplate/K8sMaxPods.yaml
 
 oc apply -f constraintTemplate/K8sMaxRequests.yaml
-oc apply -f constraints/K8sMaxRequests.yaml
 
 oc apply -f constraintTemplate/K8sReplicaSet.yaml
-oc apply -f constraints/K8sReplicaSet.yaml
 
+sleep 10
+
+oc apply -f constraints/K8sMaxPods.yaml
+oc apply -f constraints/K8sMaxRequests.yaml
+oc apply -f constraints/K8sReplicaSet.yaml
 #oc apply -f constraintTemplate/K8sRequiredLabels.yaml
 #oc apply -f constraintTemplate/NsRequiredLabel.yaml
 #oc apply -f constraints/K8sRequiredLabels.yaml
